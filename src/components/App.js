d34cd41be69567eb976c63a055bb0481836f0959
Michael Pingel <mdpingel@gmail.com>
2021-04-25 14:06:43 -0700

    add super simple bootstrap styling, change icon, remove extra files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React from 'react';
 import Header from './Header';
 import KegControl from './KegControl';
 
 function App(){
   return (
     <React.Fragment>
-      <Header />
-      <KegControl />
+      <div class='container'>
+        <Header />
+        <KegControl />
+      </div>
     </React.Fragment>
   );
 }
