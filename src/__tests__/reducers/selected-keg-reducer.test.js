6c0a0486657ed932b30138acf21fb020943ca3fc
Michael Pingel <mdpingel@gmail.com>
2021-04-30 15:49:05 -0700

    write select_keg selectedKegReducer test

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,5 +1,42 @@
 import selectedKegReducer from './../../reducers/selected-keg-reducer';
 
-test('Should return default state if there is no action type passed into the reducer', () => {
-  expect(selectedKegReducer({}, { type: null })).toEqual({});
+describe('selectedKegReducer', () => {
+
+  const currentState = {
+    'abc-123': {
+      name: 'TestBeer',
+      brand: 'Testweiser',
+      price: 6.99,
+      alcoholContent: 7,
+      pintsRemaining: 124,
+      id: 'abc-123'
+    },
+    'xyz-890': {
+      name: 'SelectBeer',
+      brand: 'Selectweiser',
+      price: 4.99,
+      alcoholContent: 99,
+      pintsRemaining: 124,
+      id: 'xyz-890'
+    }
+  }
+
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(selectedKegReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully select a keg', () => {
+    action = {
+      type: c.SELECT_KEG,
+      id: 'xyz-890'
+    };
+    expect(selectedKegReducer(currentState, action)).toEqual({
+        name: 'SelectBeer',
+        brand: 'Selectweiser',
+        price: 6.99,
+        alcoholContent: 7,
+        pintsRemaining: 124,
+        id: 'abc-123'
+    });
+  });
 });
