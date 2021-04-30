35be63337da0dee953644a533aab09b9e7b0bfe6
Michael Pingel <mdpingel@gmail.com>
2021-04-30 14:14:12 -0700

    update formVisibleOnPage to be handled by redux through a root reducer rather than state

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -10,7 +10,6 @@ class KegControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
       selectedKeg: null
     };
   }
@@ -18,13 +17,14 @@ class KegControl extends React.Component {
   handleClick = () => {
     if (this.state.selectedKeg != null) {
       this.setState({
-        formVisibleOnPage: false,
         selectedKeg: null
       });
     } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage
-      }));
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
     }
   }
 
@@ -41,9 +41,10 @@ class KegControl extends React.Component {
       id: id
     }
     dispatch(action);
-    this.setState({
-      formVisibleOnPage: false
-    });
+    const action2 = {
+      type: 'TOGGLE_FORM'
+    }
+    dispatch(action2);
   }
 
   handleChangingSelectedKeg = (id) => {
@@ -87,7 +88,7 @@ class KegControl extends React.Component {
         keg={this.state.selectedKeg}
         onClickingDelete={this.handleDeletingKeg} />
       buttonText = "Return to Keg List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
       buttonText = 'Return to Keg List';
     } else {
@@ -109,12 +110,14 @@ class KegControl extends React.Component {
 }
 
 KegControl.propTypes = {
-  mainKegList: PropTypes.object
+  mainKegList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool
 }
 
 const mapStateToProps = state => {
   return {
-    mainKegList: state
+    mainKegList: state.mainKegList,
+    formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
