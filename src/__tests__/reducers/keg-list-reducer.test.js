b310b126606b813b88d464b1f4b35a829e412733
Michael Pingel <mdpingel@gmail.com>
2021-04-30 09:09:51 -0700

    write add keg data  kegListReducer test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,41 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  
+  let action;
+  const kegData = {
+    name: 'TestBeer',
+    brand: 'Testweiser',
+    price: 6.99,
+    alcoholContent: 7,
+    remainingPints: 124,
+    id: 'abc-123'
+  };
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add ney keg data to mainKegList', () => {
+    const { name, brand, price, alcoholContent, remainingPints, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      remainingPints: remainingPints,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      remainingPints: remainingPints,
+      id: id
+      }
+    });
   });
 });
