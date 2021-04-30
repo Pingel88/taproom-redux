bac708b30f79a9c45e117e11d2d6be96e3ef5506
Michael Pingel <mdpingel@gmail.com>
2021-04-30 10:23:30 -0700

    write delete keg data kegListReducer test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,8 +1,28 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  const currentState = {
+    'abc-123': {
+      name: 'TestBeer',
+      brand: 'Testweiser',
+      price: 6.99,
+      alcoholContent: 7,
+      remainingPints: 124,
+      id: 'abc-123'
+    },
+    'xyz-890': {
+      name: 'DeleteBeer',
+      brand: 'Deleteweiser',
+      price: 4.99,
+      alcoholContent: 99,
+      remainingPints: 124,
+      id: 'xyz-890'
+    }
+  }
   
   let action;
+
   const kegData = {
     name: 'TestBeer',
     brand: 'Testweiser',
@@ -15,7 +35,7 @@ describe('kegListReducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
 
-  test('Should successfully add ney keg data to mainKegList', () => {
+  test('Should successfully add new keg data to mainKegList', () => {
     const { name, brand, price, alcoholContent, remainingPints, id } = kegData;
     action = {
       type: 'ADD_KEG',
@@ -26,7 +46,6 @@ describe('kegListReducer', () => {
       remainingPints: remainingPints,
       id: id
     };
-
     expect(kegListReducer({}, action)).toEqual({
       [id]: {
       name: name,
@@ -35,6 +54,23 @@ describe('kegListReducer', () => {
       alcoholContent: alcoholContent,
       remainingPints: remainingPints,
       id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 'xyz-890'
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      'abc-123': {
+        name: 'TestBeer',
+        brand: 'Testweiser',
+        price: 6.99,
+        alcoholContent: 7,
+        remainingPints: 124,
+        id: 'abc-123'
       }
     });
   });
